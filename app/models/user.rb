b664217c959fc1d028f8fbd100017062a8467ce1
Brice Powell <bpaulpowell@gmail.com>
2018-01-02 12:30:13 -0500

    Login

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   validates :username, uniqueness: true
   validates :password, length: {minimum: 6}, allow_nil: true
 
-  after_initilialize :ensure_session_token
+  after_initialize :ensure_session_token
 
   def self.find_by_credentials(username, password)
     user = User.find_by(username: username)
