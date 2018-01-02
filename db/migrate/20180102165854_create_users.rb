f0dacee4b3244c823afd37fd325d7620dc243fd5
Brice Powell <bpaulpowell@gmail.com>
2018-01-02 12:07:30 -0500

    Setup User model

--- db/migrate/20180102165854_create_users.rb
+++ db/migrate/20180102165854_create_users.rb
@@ -1,16 +1,18 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.string :username
-      t.string :fname
-      t.string :lname
-      t.string :password_digest
-      t.string :session_token
+      t.string :username, null: false
+      t.string :fname, null: false
+      t.string :lname, null: false
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
       t.integer :age
       t.float :height
       t.integer :weight
 
       t.timestamps
     end
+    add_index :users, :username, unique: true
+   add_index :users, :session_token, unique: true
   end
 end
