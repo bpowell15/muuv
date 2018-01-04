f6f4797fe854d9963af1a88b27a1ad711aef64f9
Brice Powell <bpaulpowel@gmail.com>
2018-01-04 18:47:28 -0500

    Time to move on

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -5,7 +5,8 @@ import {
   Redirect,
   Switch,
   Link,
-  HashRouter
+  HashRouter,
+  withRouter
 } from 'react-router-dom';
 
 import GreetingContainer from './greeting/greeting_container';
@@ -14,32 +15,56 @@ import { AuthRoute, ProtectedRoute } from '../api_util/route_util';
 
 class App extends React.Component{
   constructor(props) {
+    debugger
     super(props);
-    this.state = {
-      isOpen: false
-    };
   }
+
+  // shuffleBackground() {
+  //   const shuffleArray = arr => arr.sort(() => Math.random() - 0.5);
+  //   const backgroundArray = ['background-login', 'background-login2', 'background-login3', 'background-login4', 'background-login5' ];
+  //   return shuffleArray(backgroundArray)[0];
+  // }
+
+  title() {
+    if (this.props.location.pathname === '/login' || this.props.store.getState().session.currentUser) {
+      return <Link to="/" className="header-link"><h1>muuv</h1></Link>;
+       }
+  }
+
+  hook() {
+    if (this.props.location.pathname === '/' && this.props.store.getState().session.currentUser === null) {
+      return (
+        <div>
+          <h1 className="splash-title">muuv</h1>
+          <p className="splash-hook">Connecting athletes,
+            tracking stats.</p>
+        </div>
+        );
+    }
+  }
+
 
 
   render(){
     return (
-      <div>
-        <div className="nav-bar">
-        <header className="inner-nav-bar">
-          <Link to="/" className="header-link">
-            <h1>muuv</h1>
-          </Link>
-          <GreetingContainer />
-        </header>
+
+        <div>
+          <div className="nav-bar">
+          <header className="inner-nav-bar">
+              {this.title()}
+            <GreetingContainer />
+          </header>
+          </div>
+          <main className="main-page">
+            <AuthRoute path="/login" component={SessionFormContainer} />
+            <AuthRoute path="/signup" component={SessionFormContainer} />
+            {this.hook()}
+          </main>
         </div>
-        <main className="main-page">
-          <AuthRoute path="/login" component={SessionFormContainer} />
-          <AuthRoute path="/signup" component={SessionFormContainer} />
-        </main>
-      </div>
+
   );}
 }
 
 
 
-export default App;
+export default withRouter(App);
--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -4,10 +4,13 @@ import { HashRouter } from 'react-router-dom';
 
 import App from './app';
 
-export const Root = ({ store }) => (
-  <Provider store={store}>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </Provider>
-);
+export const Root = ({ store }) => {
+
+  return (
+    <Provider store={store}>
+      <HashRouter>
+        <App store={store}/>
+      </HashRouter>
+    </Provider>
+  )
+}
