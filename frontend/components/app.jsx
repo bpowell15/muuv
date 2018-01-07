9769890b2801a61afc729a6a8dd2fc142796a119
Brice Powell <bpaulpowell@gmail.com>
2018-01-07 13:50:34 -0500

    Add protected route, workouts/new, neccesary folder files

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -14,6 +14,7 @@ import SplashGreetingContainer from './greeting/splash_greeting_container';
 import SessionModalContainer from './session_form/session_modal_container';
 import HookGreetingContainer from './greeting/hook_greeting_container';
 import SessionFormContainer from './session_form/session_form_container';
+import WorkoutCreateForm from './workout_form/workout_form_container';
 import { AuthRoute, ProtectedRoute } from '../api_util/route_util';
 
 class App extends React.Component{
@@ -33,6 +34,9 @@ class App extends React.Component{
             <AuthRoute path="/login" component={SessionFormContainer} />
             <AuthRoute path="/signup" component={SessionFormContainer} />
             <HookGreetingContainer />
+            <ProtectedRoute path="/workouts/new"
+              component={WorkoutCreateForm} 
+            />
           </main>
         </div>
   );}
