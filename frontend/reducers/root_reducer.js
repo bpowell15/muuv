5e948e7809d1d6bc7cd29be0b3525deb2bb27b73
Brice Powell <bpaulpowell@gmail.com>
2018-01-07 13:28:30 -0500

    Implement workout util, actions, and reducers

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 
 import session from './session_reducer';
 import errors from './errors_reducer';
+import workout from './workout_reducer';
 
 const rootReducer = combineReducers({
   session,
   errors,
+  workout
 });
 
 export default rootReducer;
---
+++ frontend/reducers/workout_reducer.js
@@ -0,0 +1,21 @@
+import { RECEIVE_WORKOUTS, RECEIVE_WORKOUT, REMOVE_WORKOUT }
+  from '../actions/workout_actions';
+import merge from 'lodash/merge';
+
+
+const workoutReducer = (state = {}, action ) => {
+  let newState;
+  switch (action.type) {
+    case RECEIVE_WORKOUTS:
+      return action.workouts;
+    case RECEIVE_WORKOUT:
+      newState = merge({}, state, {[action.workout.id]: action.workout});
+      return newState;
+    case REMOVE_WORKOUT:
+      newState = merge({}, state);
+      delete newState[action.workoutID];
+      return newState;
+    default:
+      return state;
+  }
+};
