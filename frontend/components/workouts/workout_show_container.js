11974fc3052742cc6661db0d08b3a8637d45f814
bpowell15 <bpaulpowell@gmail.com>
2018-01-09 18:20:41 -0500

    Map style

--- frontend/components/workouts/workout_show_container.js
+++ frontend/components/workouts/workout_show_container.js
@@ -1,1 +1,19 @@
 import { connect } from 'react-redux';
+import WorkoutIndex from './workout_index';
+import { fetchWorkout, deleteWorkout } from '../../actions/workout_actions';
+
+const mapStateToProps = (state) => {
+  debugger
+  return {
+    user: state.session.currentUser
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchWorkout: () => dispatch(fetchWorkout()),
+    deleteWorkout: (id) => dispatch(deleteWorkout(id))
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(WorkoutIndex);
