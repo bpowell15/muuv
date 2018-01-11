110cc39c6ab8c827edd94614e469d9163476cc8c
Brice Powell <bpaulpowell@gmail.com>
2018-01-11 18:19:21 -0500

    add key

--- frontend/components/routes/route_index_item.jsx
+++ frontend/components/routes/route_index_item.jsx
@@ -10,7 +10,7 @@ class RouteIndexItem extends React.Component {
     <div className="routeItem">
       <Link to={`/routes/${this.props.route.id}`}>
         <img
-          src={`https://maps.googleapis.com/maps/api/staticmap?size=290x180&scale=1&path=weight:3%7Ccolor:fc4c02%7Cenc:${this.props.route.polyline}`}>
+          src={`https://maps.googleapis.com/maps/api/staticmap?size=290x180&scale=1&path=weight:3%7Ccolor:fc4c02%7Cenc:${this.props.route.polyline}&key=AIzaSyC3Supo7gTtIjC8R6iANlG-BVdpKOOzG38`}>
         </img>
         <div className="route-info">
           <p className="title-link">{this.props.route.title}</p>
